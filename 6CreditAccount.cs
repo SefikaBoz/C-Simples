138c76ac6170eefaf99e413831deaf900ff2b0e1
SefikaBoz <sefikaboz12@gmail.com>
2017-05-31 08:21:28 +0300

    Create 6CreditAccount.cs

---
+++ 6CreditAccount.cs
@@ -0,0 +1,19 @@
+# C-Simples
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo6
+{
+    class CreditAccount
+    {
+        public ulong AccountNo;
+        public double Limit;
+        public string CardOwner;
+    }
+    
+}
