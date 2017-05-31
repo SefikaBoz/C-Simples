002ff9a934d79b6506f81f0d436a273559a0f86b
SefikaBoz <sefikaboz12@gmail.com>
2017-05-31 08:11:12 +0300

    Create 4Program.cs

---
+++ 4Program.cs
@@ -0,0 +1,46 @@
+# C-Simples
+
+
+using System; using System.Collections.Generic; using System.Linq; using System.Text; using System.Threading.Tasks;
+
+namespace Demo4 { class Program { static void Main(string[] args) {
+
+        // ShapeClass.Shape();
+
+        //IrregularArray.IrregulararrayExmp();
+
+        //IrregularArray.IrregularExp();
+
+        //SystemArrayProperties sap = new SystemArrayProperties();
+        //sap.SystemArrayProp();
+
+        //SystemArrayProperties.SearchProp();
+
+        //SystemArrayProperties.CreateInstanceMethod();
+
+        //SystemArrayProperties.CreateInstanceMethodStr();
+
+        //SystemArrayProperties.GetUpperBoundMethod();
+
+        //SystemArrayProperties.CreateInstanceExamp();
+
+        //SystemArrayProperties.CreateInstanceExamp2();
+
+        //SystemArrayProperties.OperationCopy();
+
+        //SystemArrayProperties.SortMethod();
+
+        //SystemArrayProperties.Sort();
+
+        //SystemArrayProperties.ClearMethod();
+
+        //MinSort.Sort();
+
+        //Reverse.ToReverse();
+        Reverse.ExampleArray();
+
+
+        Console.ReadKey();
+    }
+}
+}
