47ccc756c9956fdee915ad9dc44373be931491a2
SefikaBoz <sefikaboz12@gmail.com>
2017-05-31 07:48:49 +0300

    Create ObjectDataTypes.cs

---
+++ ObjectDataTypes.cs
@@ -0,0 +1,18 @@
+# C-Simples
+
+namespace DemoCSharp1 {
+  class ObjectDataType {
+   public static void ObjectType() {
+     object a;
+      a = 5; 
+      Console.WriteLine(a.GetType()); 
+     a = "ss"; 
+     Console.WriteLine(a.GetType()); 
+     a = 'l'; 
+     Console.WriteLine(a.GetType()); 
+     a = true;
+     Console.WriteLine(a.GetType()); 
+     }
+
+  }
+}
