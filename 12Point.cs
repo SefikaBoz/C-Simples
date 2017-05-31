b3ff06c7f8b2ac9beaf4d058040168a198ed189a
SefikaBoz <sefikaboz12@gmail.com>
2017-05-31 09:43:44 +0300

    Create 12Point.cs

---
+++ 12Point.cs
@@ -0,0 +1,34 @@
+# C-Simples
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphic2D
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point()
+        {
+            Console.WriteLine("Class 2D");
+        }
+    }
+}
+namespace Graphic3D
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point()
+        {
+            Console.WriteLine("Class 3D");
+        }
+    }
+}
