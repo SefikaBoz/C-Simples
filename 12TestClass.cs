173bbb5b1ccfcee1d1cf10c99c23f9ed3c37c419
SefikaBoz <sefikaboz12@gmail.com>
2017-05-31 09:44:54 +0300

    Create 12TestClass.cs

---
+++ 12TestClass.cs
@@ -0,0 +1,22 @@
+# C-Simples
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNameSpaces
+{
+    class TestClass
+    {
+        public int Test1 { get; set; }
+        public int Test2 { get; set; }
+        public TestClass(int Test1, int Test2)
+        {
+            this.Test1 = Test1;
+            this.Test2 = Test2;
+        }
+    }
+   
+}
