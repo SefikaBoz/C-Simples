88ef0c5d694d628182f98ccd1e88d0293ec3a5c9
SefikaBoz <sefikaboz12@gmail.com>
2017-05-31 08:06:21 +0300

    Create 3Person.cs

---
+++ 3Person.cs
@@ -0,0 +1,14 @@
+# C-Simples
+
+using System; 
+using System.Collections.Generic; 
+using System.Linq; using System.Text; 
+using System.Threading.Tasks;
+
+namespace Demo3 { 
+  class Person { 
+    public string Name { get; set; } 
+    public string Surname { get; set; 
+    } 
+   } 
+}
