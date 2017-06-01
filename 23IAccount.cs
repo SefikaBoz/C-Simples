ff5e31943d4bc3e6a939d99f33196958f9e71145
SefikaBoz <sefikaboz12@gmail.com>
2017-06-01 07:17:13 +0300

    Create 23IAccount.cs

---
+++ 23IAccount.cs
@@ -0,0 +1,17 @@
+# C-Simples
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PErson
+{
+    interface IAccount
+    {
+        int AccountNumber { get; set; }
+        int AccountAmount { get; set; }
+    }
+}
