0cd3d116d514a284d380d349a27d9e280bbc418a
SefikaBoz <sefikaboz12@gmail.com>
2017-06-01 06:57:55 +0300

    Create 17Line.cs

---
+++ 17Line.cs
@@ -0,0 +1,29 @@
+# C-Simples
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Line
+    {
+        private string mStrLine;
+        public Line()
+        {
+            mStrLine = "test";
+        }
+        public void TestMethod()
+        {
+
+        }
+        static void TestStaticMethod()
+        {
+
+        }
+        
+    }
+}
